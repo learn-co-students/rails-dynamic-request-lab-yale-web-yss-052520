661959eea05c864384011bc0cb8bba7ee98ca0f6
CelestialEmperor <mapeiyao@gmail.com>
2020-06-22 02:45:33 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,20 @@
 class StudentsController < ApplicationController
+
+  # I am so incredibly confused by this line of code.
+  # Maybe it's saying that the private method set_student only works for the show method?
+  before_action :set_student, only: :show 
+
+
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
